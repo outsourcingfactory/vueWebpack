adeff1bfb9219971dd663a6e2cd79067d35fe966
mingzhi.yu <mingzhi.yu@uxin.com>
2018-05-08 19:24:48 +0800

    first

---
+++ src/components/Ymztest.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="testymz">
+        我是地瓜你是谁
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+    .testymz{
+        color: #000;
+        font-size: 50px;
+        text-align: center;
+    }
+</style>
---
+++ src/views/ymz/test/testApp.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="app">
+        嘿嘿嘿我是地瓜
+        <Ymztest></Ymztest>
+    </div>
+</template>
+
+<script>
+    import Ymztest from 'components/Ymztest';
+    export default {
+        components: {
+            Ymztest
+        }
+    }
+</script>
+
+<style lang="scss">
+    #app{
+        color: #000;
+        font-size: 50px;
+        text-align: center;
+    }
+</style>
